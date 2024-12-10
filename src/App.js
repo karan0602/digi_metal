6c713ae47a7577dfb4b78c2136fc143af9470188
Karan.k <karan.k@caratlane.com>
2024-12-10 12:05:06 +0530

    transaction list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null);
+  const [trans, setTrans] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://alchemist-xko7.onrender.com/users/1/transactions')
+    .then(response => {
+      console.log('EZ_transactions', response);
+      setTrans(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+    axios.get('https://alchemist-xko7.onrender.com/portfolio/1')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {trans?.map((item, index) => (
+        <div className="Transaction-List">
+          <h3>{item?.transactionType == 'SELL' ? 'SOLD' : 'Bought'} </h3>
+          <h3>{item?.totalAmount} </h3>
+          <h3>{item?.transactionDate}</h3>
+          
+        </div>
+      ))}
     </div>
   );
 }
